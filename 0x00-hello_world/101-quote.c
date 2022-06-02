f34f4ef0a43b13f0eb6ee353fa3e684caf977844
robelandro <robelpopandro@gmail.com>
2022-06-02 16:50:07 +0300

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,6 @@
  */
 int main(void)
 {
-    char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(1, str, 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
