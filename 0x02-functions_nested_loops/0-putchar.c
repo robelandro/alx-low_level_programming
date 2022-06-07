5828f687565b1964b1dd54e8cd08ca15abc8ed09
robelandro <robelpopandro@gmail.com>
2022-06-08 00:30:07 +0300

    update

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints _putchar
+ * Return: 0
+ */
+int main() 
+{
+	char put[] = "_putchar";
+    
+    printf("%s", put);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,4 @@
+#C - Functions, nested loops
+> #### Functions, nested loops
+[]: # Language: c
+[]: # Path: 0x02-functions_nested_loops/0-putchar.c
