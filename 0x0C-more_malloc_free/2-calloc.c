20aa52980eb4e75138ff30b98803d0eca8331427
robelpopandro <robelpopandro@gmail.com>
2022-07-03 13:10:51 +0300

    update

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (size == 0 || nmemb == 0)
 	return (NULL);
 	result = malloc(nmemb * size);
-	if (result = NULL)
+	if (result == NULL)
 	return (NULL);
 	for (i = 0; i < nmemb * size; i++)
 	result[i] = 0;
