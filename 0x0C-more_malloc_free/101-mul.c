36da6ae8d5f33ff1c669f5ea95522fc226c97d22
robelpopandro <robelpopandro@gmail.com>
2022-07-03 22:05:28 +0300

    update

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
+ */
+int main(int argc, char *argv[])
+{
+	int i,result;
+	char er[] = "Error";
+
+	if (argc != 4)
+	{
+		for (i = 0; er[i] != '\0'; i++)
+		putchar(er[i]);
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
