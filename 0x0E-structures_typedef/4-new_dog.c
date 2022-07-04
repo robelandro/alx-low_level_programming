5deffc0eacee08b78eb735f64edfdc276cfa17f3
robelpopandro <robelpopandro@gmail.com>
2022-07-04 22:26:23 +0300

    update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,46 +12,42 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *ptr_dog;
+	dog_t *p_dog;
 	int i, lname, lowner;
 
-	ptr_dog = malloc(sizeof(*ptr_dog));
-	if (ptr_dog == NULL || !(name) || !(owner))
+	p_dog = malloc(sizeof(*p_dog));
+	if (p_dog == NULL || !(name) || !(owner))
 	{
-		free(ptr_dog);
+		free(p_dog);
 		return (NULL);
 	}
 
 	for (lname = 0; name[lname]; lname++)
-	;
+		;
 
 	for (lowner = 0; owner[lowner]; lowner++)
-	;
+		;
 
-	ptr_dog->name = malloc(lname + 1);
-	ptr_dog->owner = malloc(lowner + 1);
+	p_dog->name = malloc(lname + 1);
+	p_dog->owner = malloc(lowner + 1);
 
-	if (!(ptr_dog->name) || !(ptr_dog->owner))
+	if (!(p_dog->name) || !(p_dog->owner))
 	{
-		free(ptr_dog->owner);
-		free(ptr_dog->name);
-		free(ptr_dog);
+		free(p_dog->owner);
+		free(p_dog->name);
+		free(p_dog);
 		return (NULL);
 	}
 
 	for (i = 0; i < lname; i++)
-	{
-		ptr_dog->name[i] = name[i];
-	}
-	ptr_dog->name[i] = '\0';
+		p_dog->name[i] = name[i];
+	p_dog->name[i] = '\0';
 
-	ptr_dog->age = age;
+	p_dog->age = age;
 
 	for (i = 0; i < lowner; i++)
-	{
-		ptr_dog->owner[i] = owner[i];
-	}
-	ptr_dog->owner[i] = '\0';
+		p_dog->owner[i] = owner[i];
+	p_dog->owner[i] = '\0';
 
-	return (ptr_dog);
+	return (p_dog);
 }
