618ea3ccf87845fe2bd73249e1081fb8fb9af596
Robel Pop <robelpopandro@gmail.com>
2022-11-17 19:49:53 +0300

    Technical interview preparation

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+contains function that calculates island perimeter
+"""
+
+
+def island_perimeter(grid):
+    """returns the perimeter of the island described in grid
+    grid: 2d list of integer - 1 land zone & 0 water zone"""
+
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                try:
+                    if grid[i - 1][j] == 0 and grid[i + 1][j] == 0\
+                       and grid[i][j - 1] == 0 and grid[i][j + 1] == 0:
+                        perimeter += 4
+                        continue
+                except IndexError:
+                    return
+                if i == 0 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
+                    perimeter += 1
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
+                    perimeter += 1
+    return perimeter
