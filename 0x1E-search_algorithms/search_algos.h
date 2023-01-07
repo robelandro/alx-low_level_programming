0006d6bd073976c5d44b36b6b295a9f0cccfafe2
Robel Pop <robelpopandro@gmail.com>
2023-01-07 16:47:55 +0300

    fixed

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -48,11 +48,6 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
-int binary_search_helper(int *array, size_t left, size_t right, int value);
-int jump_search_helper(int *array, size_t size, int value, size_t step);
-int interpolation_search_helper(int *array, size_t size, int value, size_t low, size_t high);
-int exponential_search_helper(int *array, size_t size, int value, size_t bound);
-int advanced_binary_helper(int *array, size_t left, size_t right, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 
